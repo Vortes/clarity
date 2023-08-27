665b01b1b0c59338635d724e701319644c474911
Alan <zeyu.weng@gmail.com>
2023-08-27 18:56:17 -0400

    extension up and running

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="">
+      <header className="App-header">
+        <p className="text-2xl">
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
