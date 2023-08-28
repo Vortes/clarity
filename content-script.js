71de346cfda0b9b2f8fe5ca16795e4d48f439e6f
Alan <zeyu.weng@gmail.com>
2023-08-28 18:26:53 -0400

    storage implemented

--- content-script.js
+++ content-script.js
@@ -1,4 +1,5 @@
 chrome.runtime.onMessage.addListener(request => {
     console.log(request.data)
+    console.log(request.tabURL)
     }
 );
--- popup.js
+++ popup.js
@@ -1,10 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("getUrl").addEventListener("click", async () => {
-        const data = document.getElementById("input").value
-        document.getElementById("list").innerHTML = data
 
+    const watchLinksList = document.getElementById("list")
+    const button = document.getElementById("getUrl")
+
+    chrome.storage.sync.get(["links"], result => {
+        if (result.links) {
+            result.links.forEach((link) => {
+                addLinkToList(link)
+            })
+        }
+    })
+
+    button.addEventListener("click", async () => {
+
+        const data = document.getElementById("input").value
+
+        console.log(data)
+        addLinkToList(data)
+        saveLink(data)
+        data = ""
+        
         // send tab url & form input
         const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-        await chrome.tabs.sendMessage(tab.id, {data: data});
+        await chrome.tabs.sendMessage(tab.id, {data: data, tabURL: tab.url});
     })
+
+    const addLinkToList = async (link) => {
+        const li = document.createElement("li")
+        li.innerHTML = link
+        watchLinksList.appendChild(li)
+    }
+
+    const saveLink = (link) => {
+        chrome.storage.sync.get(["links"], result => {
+        const links = result.links || []
+        links.push(link)
+        chrome.storage.sync.set({links: links})
+        })
+    }
 });
