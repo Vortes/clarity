48385553cf681361260c6ab096c9acbc02bfed60
Alan <zeyu.weng@gmail.com>
2023-09-08 09:23:19 -0400

    doesnt matter if it has www or not, user can also use the format website.com instead of https://www.website.com

--- background.js
+++ background.js
@@ -5,30 +5,31 @@ const getLinks = async () => {
 
 // helper function that allows me to extract base domain from links like https://www.baseDomain.com/placeholder/placeholder
 const extractBaseDomain = (url) => {
-    // Remove the "https://" or "http://" part from the URL
-    url = url.replace(/(https?:\/\/)?(www\.)?/, '');
-  
-    // Split the URL by "/" to get an array of parts
-    const parts = url.split('/');
-  
-    // The base domain is the first part of the split URL
-    const baseDomain = parts[0];
-  
-    return baseDomain;
+  // Remove the "https://" or "http://" part from the URL
+  url = url.replace(/(https?:\/\/)?/, '');
+
+  // Remove "www." if it exists at the beginning of the URL
+  url = url.replace(/^(www\.)?/, '');
+
+  // Split the URL by "/" to get an array of parts
+  const parts = url.split('/');
+
+  // The base domain is the first part of the split URL
+  const baseDomain = parts[0];
+
+  return baseDomain;
   }
 
 const handleTabActivation = async () => {
     const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
     const url = tab.url;
-    // console.log(url)
+    const baseDomain = extractBaseDomain(url)
     const tabId = tab.id
     
     // Check to see if current URL matches any URL in the storage
     const result = await getLinks();
-    // console.log(result)
 
-    const baseDomain = extractBaseDomain(url)
-    console.log(baseDomain)
+    console.log("https://www."+ baseDomain+"/")
 
     if (result.hasOwnProperty(baseDomain)) {
       chrome.tabs.sendMessage(tabId, { status: 'validated' });
