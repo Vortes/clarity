056d93b908d39bce3f020c9dc240efe63e75093a
Alan <zeyu.weng@gmail.com>
2023-09-08 09:12:56 -0400

    able to extract base domain

--- background.js
+++ background.js
@@ -3,18 +3,34 @@ const getLinks = async () => {
     return result
 }
 
+// helper function that allows me to extract base domain from links like https://www.baseDomain.com/placeholder/placeholder
+const extractBaseDomain = (url) => {
+    // Remove the "https://" or "http://" part from the URL
+    url = url.replace(/(https?:\/\/)?(www\.)?/, '');
+  
+    // Split the URL by "/" to get an array of parts
+    const parts = url.split('/');
+  
+    // The base domain is the first part of the split URL
+    const baseDomain = parts[0];
+  
+    return baseDomain;
+  }
+
 const handleTabActivation = async () => {
     const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
     const url = tab.url;
-    console.log(url)
+    // console.log(url)
     const tabId = tab.id
     
     // Check to see if current URL matches any URL in the storage
     const result = await getLinks();
-    console.log(result.hasOwnProperty(url))
-  
-    // If it matches, send a message to the tab
-    if (result.hasOwnProperty(url)) {
+    // console.log(result)
+
+    const baseDomain = extractBaseDomain(url)
+    console.log(baseDomain)
+
+    if (result.hasOwnProperty(baseDomain)) {
       chrome.tabs.sendMessage(tabId, { status: 'validated' });
       return true
     }
